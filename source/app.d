c30e557c4d78bda2516e67c6f7d3bccd9c250552
Navid M <navidmomtahen@gmail.com>
2025-05-30 21:46:37 +0100

    update

--- source/app.d
+++ source/app.d
@@ -2,35 +2,90 @@ import std.stdio;
 import std.socket;
 import std.string;
 import std.conv;
+import std.algorithm;
+import std.array;
+import std.exception;
+import std.functional;
+import std.typecons;
+import std.format;
+
+alias RouteHandler = string delegate();
+
+class PrismApplication
+{
+	private TcpSocket server;
+	private string[string] routes;
+
+	this(ushort port = 8080)
+	{
+		server = new TcpSocket();
+		server.bind(new InternetAddress(port));
+		server.listen(10);
+	}
+
+	void get(string path, RouteHandler handler)
+	{
+		routes[path] = handler();
+	}
+
+	void run()
+	{
+		writeln("Server running at http://localhost:8080");
+		scope (exit)
+			server.close();
+
+		while (true)
+		{
+			auto client = server.accept();
+			scope (exit)
+				client.close();
+
+			ubyte[4096] buffer;
+			auto bytesRead = client.receive(buffer);
+			if (bytesRead <= 0)
+				continue;
+
+			auto request = cast(string) buffer[0 .. bytesRead];
+			writeln("Received request:\n", request);
+
+			auto path = extractPath(request);
+			auto responseBody = handleRoute(path);
+
+			string response = format(
+				"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: %s\r\nConnection: close\r\n\r\n%s",
+				responseBody.length, responseBody
+			);
+
+			client.send(cast(ubyte[]) response);
+		}
+	}
+
+	private string extractPath(string request)
+	{
+		auto lines = request.splitLines();
+		if (lines.length == 0)
+			return "/";
+		auto requestLine = lines[0].split();
+		if (requestLine.length >= 2)
+			return requestLine[1];
+		return "/";
+	}
+
+	private string handleRoute(string path)
+	{
+		if (auto handler = path in routes)
+			return (*handler);
+		return "<html><body><h1>404 Not Found</h1></body></html>";
+	}
+}
 
 void main()
 {
-	writeln("Go to http://localhost:8080.");
+	auto app = new PrismApplication();
 
-	auto server = new TcpSocket();
-	server.bind(new InternetAddress(8080));
-	server.listen(10);
+	app.get("/", () => "<html><body><h1>Welcome to D Prism Framework</h1></body></html>");
+	app.get("/about", () => "<html><body><h1>About Page</h1></body></html>");
+	app.get("/hello", () => "<html><body><h1>Hello World!</h1></body></html>");
 
-	scope (exit)
-		server.close();
-
-	while (true)
-	{
-		auto client = server.accept();
-		scope (exit)
-			client.close();
-
-		ubyte[2056] buffer;
-		auto bytesRead = client.receive(buffer);
-		auto request = cast(string) buffer[0 .. bytesRead];
-		writeln("Received request:\n", request);
-		string bodya = "<html><body><h1>Hello from D!</h1></body></html>";
-		string response = "HTTP/1.1 200 OK\r\n" ~
-			"Content-Type: text/html\r\n" ~
-			"Content-Length: " ~ to!string(
-				bodya.length
-			) ~ "\r\n" ~
-			"Connection: close\r\n\r\n" ~ bodya;
-		client.send(cast(ubyte[]) response);
-	}
+	app.run();
 }
